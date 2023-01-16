7322f5a1178180e1639fe52311649553ee345dd7
Dylan Wright <17WrighD@fulford.york.sch.uk>
2023-01-16 11:17:44 +0000

    SCC

--- lib.js
+++ lib.js
@@ -1,14 +1,35 @@
 // metronome
 console.log("Setting up metronome");
 const btnDecrease = document.getElementById("btnDecreaseBPM")
-const btnInecrease = document.getElementById("btnIncreaseBPM")
+const btnIncrease = document.getElementById("btnIncreaseBPM")
+const bpmInput = document.getElementById("txtBPM");
 
 //beats mer minute
 var bpm = 60;
 
+bpmInput.onchange = function (e) {
+    bpm = parseInt(bpmInput.value);
+    if (isNaN(bpm)) {
+        bpm = 50;
+    }
+    console.log(bpm);
+}
 
 btnDecreaseBPM.onclick = function (e) {
     bpm -= 1
+    console.log("bpm")
+    updateBPM();
+
+}
+
+btnIncreaseBPM.onclick = function (e) {
+    bpm += 1
+    console.log("bpm")
+    updateBPM();
+
+}
+function updateBPM() {
+    bpmInput.value = bpm
 
 }
 
